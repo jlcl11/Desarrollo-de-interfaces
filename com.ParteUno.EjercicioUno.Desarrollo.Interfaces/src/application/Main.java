b1e4df40df4607bbd19b450e3e704444bba84893
jlcl11 <jcorrallopez8@gmail.com>
2022-11-07 12:31:13 +0100

    Sobreescribi el ejercicio 2 con el ej1
    Hice el ejercicio 1,y escribi el ejercicio 2 sobre el ejercicio 1 por lo que no se me ha guardado :angry:

--- com.ParteUno.EjercicioUno.Desarrollo.Interfaces/src/application/Main.java
+++ com.ParteUno.EjercicioUno.Desarrollo.Interfaces/src/application/Main.java
@@ -8,17 +8,22 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
+	public void start(Stage primaryStage) {try {
+		StackPane panel = new StackPane();
+		
+		Rectangle r1 = new Rectangle(400,400,Color.DARKGREEN);
+		Rectangle r2 = new Rectangle(300,300,Color.GREEN);
+		Rectangle r3 = new Rectangle(200,200,Color.LIGHTGREEN);
+		
+		panel.getChildren().addAll(r1, r2, r3);
+		
+		Scene scene = new Scene(panel,400,400);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	} catch(Exception e) {
+		e.printStackTrace();
+	}}
 	
 	public static void main(String[] args) {
 		launch(args);
