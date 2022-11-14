a5e9d6c0980878f0f270cb77e1c31d4ca04537aa
jlcl11 <jcorrallopez8@gmail.com>
2022-11-14 13:06:20 +0100

    14/11/2022 Antes del descanso

--- prueba SceneBuilder/src/application/SampleController.java
+++ prueba SceneBuilder/src/application/SampleController.java
@@ -1,5 +1,26 @@
 package application;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class SampleController {
-	
+
+	@FXML
+	private TextField txtMensaje;
+
+	@FXML
+	private Button btnEnviar;
+
+	@FXML
+	private Label lblMensaje;
+
+	@FXML
+	public void mostrarMensaje(ActionEvent event) {
+
+		lblMensaje.setText(txtMensaje.getText());
+
+	}
 }
